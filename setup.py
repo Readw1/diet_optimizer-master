a0b12247f8a37ef26b695e6223a84d59875f4b15
Read <readbrown1@10-21-161-66.dynapool.wireless.nyu.edu>
2019-04-14 10:49:59 -0400

    modify requirement

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='diet_optimizer',
+      version='1.0',
+      description='Diet Optimizer',
+      author='Read Brown',
+      author_email='rwb319@nyu.edu',
+      url='https://obscure-wildwood-73671.herokuapp.com/',
+      packages=['diet_optimizer'],
+     )
